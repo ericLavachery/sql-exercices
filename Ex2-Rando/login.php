a9c08ad3fe2bcdca46e74442743773929a0a8a78
ericLavachery <eric.lavachery@gmail.com>
2018-08-17 15:23:19 +0200

    ex2-rando auth + table customers + sha1

--- Ex2-Rando/login.php
+++ Ex2-Rando/login.php
@@ -1,16 +1,39 @@
 <?php
 $teou = 'Login';
 $formMessage = '';
-
-// On définit un login et un mot de passe de base pour tester notre exemple. Cependant, vous pouvez très bien interroger votre base de données afin de savoir si le visiteur qui se connecte est bien membre de votre site
-$login_valide = "moi";
-$pwd_valide = "lemien";
+$log = FALSE;
 
 // on teste si nos variables sont définies
 if (isset($_POST['login']) && isset($_POST['pwd'])) {
 
+    $servername = "localhost";
+    $username = "root";
+    $password = "zen8070\$mysql";
+    $myDB = "reunion_island";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$myDB;charset=utf8", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully";
+        $stmt = $conn->prepare("SELECT * FROM customers");
+        $stmt->execute();
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $myTable = '';
+        foreach($stmt->fetchAll() as $k=>$v) {
+            if ($v['login'] == $_POST['login'] && $v['pwd'] == sha1($_POST['pwd'])) {
+                $log = TRUE;
+            }
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
 	// on vérifie les informations du formulaire, à savoir si le pseudo saisi est bien un pseudo autorisé, de même pour le mot de passe
-	if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
+	if ($log) {
 
 		session_start ();
 		$_SESSION['login'] = $_POST['login'];
