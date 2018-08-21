95b531394eb62ecfb15fa31f0c54b6b1748bd695
ericLavachery <eric.lavachery@gmail.com>
2018-08-21 16:34:26 +0200

    ex5-crud update client

--- Ex4-CRUD/_form.php
+++ Ex4-CRUD/_form.php
@@ -11,15 +11,37 @@
     <input type="text" name="birthDate" value="<?= $formBD ?>" placeholder="jj/mm/aaaa">
 </div>
 <div>
-    <label for="card">Carte de fidélité</label>
-    <select name="card">
+    <label for="card">Carte</label>
+    <select name="card" onchange="showDiv(this)" id="card">
         <option value="0" selected></option>
         <option value="1"<?php if ($card == 1) {echo ' selected';} ?>>Oui</option>
         <option value="0"<?php if ($card == 0) {echo ' selected';} ?>>Non</option>
     </select>
 </div>
-<div>
+<div id="showIfCard">
     <label for="cardNumber">N° de carte</label>
     <input type="text" name="cardNumber" value="<?= $cardNumber ?>" maxlength="4">
 </div>
+<div id="viewIfCard">
+    <label for="cardTypesId">Type de carte</label>
+    <select name="cardTypesId">
+        <option value="" selected></option>
+        <option value="1"<?php if (isset($cardTypesId) && $cardTypesId == 1) {echo ' selected';} ?>>Fidélité</option>
+        <option value="2"<?php if (isset($cardTypesId) && $cardTypesId == 2) {echo ' selected';} ?>>Famille Nombreuse</option>
+        <option value="3"<?php if (isset($cardTypesId) && $cardTypesId == 3) {echo ' selected';} ?>>Etudiant</option>
+        <option value="4"<?php if (isset($cardTypesId) && $cardTypesId == 4) {echo ' selected';} ?>>Employé</option>
+    </select>
+</div>
 <label for="button">&nbsp;</label>
+
+<script type="text/javascript">
+function showDiv(elem){
+    if (elem.value == 1) {
+        document.getElementById('showIfCard').style.display = "block";
+        document.getElementById('viewIfCard').style.display = "block";
+    } else {
+        document.getElementById('showIfCard').style.display = "none";
+        document.getElementById('viewIfCard').style.display = "none";
+    }
+}
+</script>
--- Ex4-CRUD/update_client.php
+++ Ex4-CRUD/update_client.php
@@ -1,7 +1,10 @@
 <?php
-$teou = 'Ajouter Client';
+$teou = 'Modifier Client';
 $formMessage = '';
 
+$modID = $_GET['id'];
+
+$id = '';
 $firstName = '';
 $lastName = '';
 $birthDate = '';
@@ -20,7 +23,7 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     if (isset($_POST["button"])) {
-        $cardTypesId = $_POST['cardTypesId'];
+        $id = $_POST['id'];
         $firstName = $_POST['firstName'];
         $lastName = $_POST['lastName'];
         $formBD = $_POST['birthDate'];
@@ -35,6 +38,7 @@ try {
             $cardTypesId = null;
         }
         $tab = array(
+            ':id' => $id,
             ':firstName' => $firstName,
             ':lastName' => $lastName,
             ':birthDate' => $birthDate,
@@ -42,11 +46,25 @@ try {
             ':cardNumber' => $cardNumber,
             ':cardTypesId' => $cardTypesId
         );
-        $sql = "INSERT INTO clients (firstName, lastName, birthDate, card, cardNumber) VALUES (:firstName, :lastName, :birthDate, :card, :cardNumber)";
+        $sql = "UPDATE clients SET firstName=:firstName, lastName=:lastName, birthDate=:birthDate, card=:card, cardNumber=:cardNumber WHERE id=:id";
         $stmt = $conn->prepare($sql);
         $stmt->execute($tab);
-        $formMessage = 'Le nouveau a bien été ajouté';
+        $formMessage = 'Le client a bien été modifié';
         // echo "New record created successfully";
+    } else {
+        $stmt = $conn->prepare("SELECT * FROM clients WHERE id = $modID");
+        $stmt->execute();
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach($stmt->fetchAll() as $k=>$v) {
+            $firstName = $v['firstName'];
+            $lastName = $v['lastName'];
+            $birthDate = $v['birthDate'];
+            $card = $v['card'];
+            $cardNumber = $v['cardNumber'];
+        }
+        $bdate = date_create_from_format('Y-m-d', $birthDate);
+        $formBD = date_format($bdate, 'd/m/Y');
     }
 }
 catch(PDOException $e)
@@ -69,8 +87,9 @@ $conn = null;
 
     <h1>Ajouter un CLIENT</h1>
         <form action="" method="post">
+            <input type="hidden" name="id" value="<?= $modID ?>">
             <?php include('_form.php'); ?>
-            <button type="submit" name="button">Ajouter</button>
+            <button type="submit" name="button">Modifier</button>
         </form>
         <p class="rouge"><?= $formMessage ?></p>
 
